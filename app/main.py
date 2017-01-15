684ad00e9ec17a8eb2c2aa3c1ef26f58dca55777
47_TT <miraculousjade@gmail.com>
2017-01-16 00:05:04 +0900

    実装

--- app/main.py
+++ app/main.py
@@ -1,4 +1,129 @@
-#!/usr/bin/env python
+# coding: utf-8
+import sys
+import json
+import datetime
+from datetime import datetime as dt
+import urllib
+import urllib2
+import xml.etree.ElementTree as ET
+import scipy.stats
+
 def main(argv):
-  for v in argv:
-    print(v)
+    #jsonを用いてキーワードをパース
+    jsonString = u'{"keywords":' + argv[0] + u'}'
+    jsonComponent = json.loads(jsonstring)
+    keywordNumber = len(jsonComponent[u'keywords'])
+
+    #開始日時と終了日時をパース
+    startDate = dt.strptime(argv[1], u'%Y-%m-%d')
+    endDate = dt.strptime(argv[2], u'%Y-%m-%d')
+
+    #件数保存用の配列を初期化
+    numbersArray = []
+    for i in range(0, keywordNumber):
+        numbersArray.append([])
+
+    #記事検索
+    weekStartDate = startDate
+    while True:
+        #検索期間の設定
+        weekEndDate = weekStartDate + datetime.timedelta(days=6)
+        if (weekEndDate > endDate):
+            #端数が出たら無視
+            weekEndDate = endDate
+            break
+
+        #記事検索
+        for i in range(0, keywordNumber):
+            #キーワードと検索期間からクエリを作成
+            v = jsonComponent[u'keywords'][i]
+            urlprefix = 'http://54.92.123.84/search?'
+            query = [
+                ('ackey', '869388c0968ae503614699f99e09d960f9ad3e12'),
+                ('q', 'Body:' + urllib.quote(v) + '%20AND%20' + 'ReleaseDate:[' + weekStartDate.strftime('%Y-%m-%d') + '%20TO%20' + weekEndDate.strftime('%Y-%m-%d') + ']'),
+            ]
+
+            #URLの形に整形
+            url = urlprefix
+            for item in query:
+                url += item[0] + "=" + item[1] + "&"
+            url = url[:-1]
+
+            #レスポンスを取得
+            response = urllib2.urlopen(url)
+            resdata = response.read()
+
+            #XMLを解析して件数を取得
+            root = ET.fromstring(resdata)
+            result = root.find('.//result')
+            numbersArray[i].append(int(result.get('numFound')))
+
+        #次の検索期間へ
+        weekStartDate = weekEndDate + datetime.timedelta(days=1)
+
+    #相関係数保存用の配列を初期化
+    coefficientsArray = []
+    for i in range(0, keywordNumber):
+        coefficientsArray.append([])
+
+    #相関係数の計算
+    for i in range(0, keywordNumber):
+        for j in range(0, keywordNumber):
+            coefficientsArray[i].append(scipy.stats.pearsonr(numbersArray[i], numbersArray[j]))
+
+    #形態要素解析
+    posArray = []
+    for i in range(0, keywordNumber):
+        #キーワードからクエリを作成
+        v = jsonComponent[u'keywords'][i]
+        urlprefix = "http://jlp.yahooapis.jp/MAService/V1/parse?"
+        query = [
+            ('appid', 'dj0zaiZpPU84QnlScDdxM1p5NSZzPWNvbnN1bWVyc2VjcmV0Jng9MjY-'),
+            ('results', 'ma'),
+            ('sentence', urllib.quote(v))
+        ]
+
+        #URLの形に整形
+        url = urlprefix
+        for item in query:
+            url += item[0] + "=" + item[1] + "&"
+        url = url[:-1]
+
+        #レスポンスを取得
+        response = urllib2.urlopen(url)
+        resdata = response.read()
+
+        #XMLを解析して品詞を取得
+        root = ET.fromstring(resdata)
+        result = root.find('.//{urn:yahoo:jp:jlp}pos')
+        posArray.append(result.text)
+
+    #品詞がすべて等しいかを確認
+    posChecker = True
+    for i in range(1, keywordNumber):
+        if (posArray[0] != posArray[1]):
+            posChecker = False
+
+    #出力を整形
+    string = '{"coefficients":['
+    for i in range(0, keywordNumber):
+        string += '['
+        for j in range(0, keywordNumber):
+            if (i == j):
+                #同じ要素なら"1"を出力
+                string += "1"
+            else:
+                #違う要素なら相関係数を小数点以下3桁で出力
+                string += str(round(coefficientsArray[i][j][0],3))
+            string += ','
+        string    = string[:-1]
+        string += '],'
+    string    = string[:-1]
+    string += '],"posChecker":'
+    #品詞がすべて等しいかを出力
+    string += str(posChecker)
+    string += '}'
+
+    #出力
+    print(string)
+    
--- index.py
+++ index.py
@@ -1,5 +1,11 @@
-#!/usr/bin/env python
+# coding: utf-8
+
 import sys
 from app.main import main
 
+#デフォルトのエンコーディングをUTF-8に設定
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+#実行
 main(sys.argv[1:])
