156a1415788bd77e9430d2a2fdc86c60c223f689
47_TT <miraculousjade@gmail.com>
2017-01-16 11:31:46 +0900

    さらにクエリを減らす

--- app/main.py
+++ app/main.py
@@ -13,6 +13,11 @@ def main(argv):
     jsonString = u'{"keywords":' + argv[0].decode('utf-8') + u'}'
     jsonComponent = json.loads(jsonString)
     keywordNumber = len(jsonComponent[u'keywords'])
+ 
+    #UTF-8のキーワードリストを作成
+    Keywords = []
+    for i in range(0, keywordNumber):
+        Keywords.append(jsonComponent[u'keywords'][i].encode('utf-8'))
 
     #開始日時と終了日時をパース
     startDate = dt.strptime(argv[1], u'%Y-%m-%d')
@@ -34,48 +39,48 @@ def main(argv):
         for j in range(0, weekNum):
             numbersArray[i].append(0)
 
-    #記事検索
-    for i in range(0, keywordNumber):
-        v = jsonComponent[u'keywords'][i]
+    #クエリのプレフィックスを作成
+    urlprefix  = 'http://54.92.123.84/search?'
+    urlprefix += 'ackey=869388c0968ae503614699f99e09d960f9ad3e12&'
+    urlprefix += 'q=Body:' + Keywords[0]
+    for i in range(1, keywordNumber):
+        urlprefix += ' OR Body:' + Keywords[i]
+    urlprefix += ' AND ReleaseDate:[' + startDate.strftime('%Y-%m-%d') + '%20TO%20' + endDate.strftime('%Y-%m-%d') + ']&'
+    urlprefix += 'rows=100&'
+    urlprefix = urllib.quote(urlprefix)
 
-        #件数を取得
-        index = 0
-        while True:
-            #キーワードと検索期間からクエリを作成
-            urlprefix = 'http://54.92.123.84/search?'
-            query = [
-                ('ackey', '869388c0968ae503614699f99e09d960f9ad3e12'),
-                ('q', 'Body:' + urllib.quote(v.encode('utf-8')) + '%20AND%20' + 'ReleaseDate:[' + startDate.strftime('%Y-%m-%d') + '%20TO%20' + endDate.strftime('%Y-%m-%d') + ']'),
-                ('start', str(index)),
-                ('rows', '100'),
-            ]
-            
-            #URLの形に整形
-            url = urlprefix
-            for item in query:
-                url += item[0] + "=" + item[1] + "&"
-            url = url[:-1]
+    #件数を取得
+    number = -1
+    index = 0
+    while True:
+        #クエリを作成
+        url = urlprefix + 'start=' + str(index)
 
-            #レスポンスを取得
-            response = urllib2.urlopen(url)
-            resdata = response.read()
+        #レスポンスを取得
+        response = urllib2.urlopen(url)
+        resdata = response.read()
 
-            #XMLを解析して件数を取得
-            root = ET.fromstring(resdata)
+        #XMLを解析して件数を取得
+        root = ET.fromstring(resdata)
+        if (number == -1):
             result = root.find('.//result')
             number = int(result.get('numFound'))
 
-            #XMLを解析して週別の件数を取得
-            root = ET.fromstring(resdata)
-            for e in root.getiterator("ReleaseDate"):
-                releseDate = dt.strptime(e.text, u'%Y-%m-%d')
-                week = (releseDate - startDate).days // 7
-                numbersArray[i][week] += 1
+        #XMLを解析して週別の件数を取得
+        for e in root.getiterator('doc'):
+            rd = e.find('.//ReleseDate')
+            releseDate = dt.strptime(rd.text, u'%Y-%m-%d')
+            week = (releseDate - startDate).days // 7
 
-            #インデックスを更新
-            if (index > number):
-                break
-            index += 100
+            body = e.find('.//Body')
+            for i in range(0, keywordNumber):
+                if (Keywords[i] in body.text):
+                    numbersArray[i][week] += 1
+
+        #インデックスを更新
+        if (index > number):
+            break
+        index += 100
 
     #相関係数保存用の配列を初期化
     coefficientsArray = []
@@ -85,49 +90,50 @@ def main(argv):
     #相関係数の計算
     for i in range(0, keywordNumber):
         for j in range(0, keywordNumber):
-            coefficientsArray[i].append(numpy.corrcoef(numbersArray[i], numbersArray[j])[0, 1])
+            if (i != j):
+                coefficientsArray[i].append(numpy.corrcoef(numbersArray[i], numbersArray[j])[0, 1])
+            else:
+                coefficientsArray[i].append(1)
 
-    #形態要素解析用の配列を初期化
-    posArray = []
-    for i in range(0, keywordNumber):
-        posArray.append([])
 
     #形態要素解析
-    for i in range(0, keywordNumber):
-        #キーワードからクエリを作成
-        v = jsonComponent[u'keywords'][i]
-        urlprefix = "http://jlp.yahooapis.jp/MAService/V1/parse?"
-        query = [
-            ('appid', 'dj0zaiZpPU84QnlScDdxM1p5NSZzPWNvbnN1bWVyc2VjcmV0Jng9MjY-'),
-            ('results', 'ma'),
-            ('sentence', urllib.quote(v.encode('utf-8')))
-        ]
+    posArray = []
 
-        #URLの形に整形
-        url = urlprefix
-        for item in query:
-            url += item[0] + "=" + item[1] + "&"
-        url = url[:-1]
+    #キーワードからクエリを作成
+    urlprefix = "http://jlp.yahooapis.jp/MAService/V1/parse?"
+    sentence = Keywords[0]
+    for i in range(1, keywordNumber):
+        sentence += ',' + Keywords[i]
+    query = [
+        ('appid', 'dj0zaiZpPU84QnlScDdxM1p5NSZzPWNvbnN1bWVyc2VjcmV0Jng9MjY-'),
+        ('results', 'ma'),
+        ('sentence', urllib.quote(sentence.encode('utf-8')))
+    ]
 
-        #レスポンスを取得
-        response = urllib2.urlopen(url)
-        resdata = response.read()
+    #URLの形に整形
+    url = urlprefix
+    for item in query:
+        url += item[0] + "=" + item[1] + "&"
+    url = url[:-1]
 
-        #XMLを解析して品詞を取得
-        root = ET.fromstring(resdata)
-        for e in root.getiterator('{urn:yahoo:jp:jlp}pos'):
-            posArray[i].append(e.text)
+    #レスポンスを取得
+    response = urllib2.urlopen(url)
+    resdata = response.read()
+
+    #XMLを解析して品詞を取得
+    root = ET.fromstring(resdata)
+    for e in root.getiterator('{urn:yahoo:jp:jlp}word'):
+        surface = e.find('.//{urn:yahoo:jp:jlp}surface')
+        if (surface.text != ','):
+            pos = e.find('.//{urn:yahoo:jp:jlp}pos')
+            posArray.append(pos.text)
 
     #品詞がすべて等しいかを確認
     posChecker = True
-    for i in range(1, keywordNumber):
-        if (len(posArray[0]) == len(posArray[i])):
-            for j in range(0, len(posArray[0])):
-                if (posArray[0][j] != posArray[i][j]):
-                    posChecker = False
-        else:
+    for pos in posArray:
+        if (posArray[0] != pos):
             posChecker = False
-
+ 
     #出力を整形
     string = '{"coefficients":['
     for i in range(0, keywordNumber):
